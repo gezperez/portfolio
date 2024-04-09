662a9bb9120b17e138f3f3bd94d03defaf072264
Ezequiel Perez <ezequielperezpc@gmail.com>
2024-04-09 16:02:02 -0300

    Add side bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import { Sidebar } from "@/components";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
 
-export const inter = Montserrat({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ezequiel Perez Portfolio",
@@ -16,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className={`${inter.className} antialiased`}>{children}</main>
+        <main className={`${montserrat.className} antialiased`}>
+          {children}
+        </main>
       </body>
     </html>
   );
